8015c81cacfe696b6b173b2e677065d980278f7c
Lukes-Github-Account <130054037+Lukes-Github-Account@users.noreply.github.com>
2023-12-08 20:44:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 
 import streamlit
 import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -25,7 +29,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # write your own comment -what does the next line do? 
@@ -34,7 +37,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
-import snowflake.connector
+streamlit.stop()
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -46,3 +50,5 @@ streamlit.dataframe(my_data_rows)
 
 fruit_choice2 = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding ', fruit_choice2)
+
+my_cur.execute("insert into fruit_load_list vales ('from streamlit')")
